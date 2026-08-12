256ed1ec79ac8b24472cab1d2f832c212866080d
cwpark <beamwidth@naver.com>
2026-08-13 07:38:43 +0900

    2026,8,13 방재실 첫 커밋

---
+++ lib/const/color.dart
@@ -0,0 +1,5 @@
+
+import 'package:flutter/material.dart';
+
+
+const primaryColor=Color(0xFF0DB2B2);
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime? selectedDay;
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +19,35 @@ class HomeScreen extends StatelessWidget {
             focusedDay: DateTime(2026,8,12),
             firstDay: DateTime(1800),
             lastDay: DateTime(3000),
+          onDaySelected: (DateTime selectedDayK, DateTime focusedDayK){
+              print(selectedDayK);
+              print(focusedDayK);
+
+              setState(() {
+                this.selectedDay=selectedDayK;
+              });
+        },
+          selectedDayPredicate: (DateTime date ){
+
+              if(selectedDay==null){
+                return false;
+              }
+
+              return date.isAtSameMomentAs(selectedDay!);
+          },
+
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+            titleTextStyle: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+
+          calendarStyle: CalendarStyle(
+            
+          ),
         ),
       ),
     );
