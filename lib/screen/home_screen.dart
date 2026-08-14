0eb5979d750b78400935d03ac920dca115ca9041
changwonpark_macMini <beamwidth@naver.com>
2026-08-15 06:45:36 +0900

    2026,8,15 맥미니 06:45

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cw_calendar_tr11/const/color.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -23,13 +24,37 @@ class _HomeScreenState extends State<HomeScreen> {
             focusedDay: DateTime(2026,8,12),
             firstDay: DateTime(1800),
             lastDay: DateTime(3000),
-          onDaySelected: (DateTime selectedDay, DateTime focusedDay){
+          onDaySelected: (DateTime selectedDayK, DateTime focusedDay){
               print(selectedDay);
               print(focusedDay);
+
+              setState(() {
+                this.selectedDay=selectedDayK;
+              });
           },
-          selectedDayPredicate: (DateTime time){
-              return true;
+          selectedDayPredicate: (DateTime date){
+              if(selectedDay==null){
+                return false;
+              }
+
+              return date.isAtSameMomentAs(selectedDay!);
           },
+
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+            titleTextStyle: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w700,
+            )
+          ),
+
+          calendarStyle: CalendarStyle(
+            isTodayHighlighted: true,
+            defaultDecoration: BoxDecoration(
+              border: Border.all(color: primaryColor, width: 3.0),
+            )
+          ),
         ),
       ),
     );
