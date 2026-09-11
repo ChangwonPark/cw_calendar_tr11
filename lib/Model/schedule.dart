723e5d9c0de5b72ec864935223e27dc2e3aa53af
cwpark <beamwidth@naver.com>
2026-09-12 08:42:49 +0900

    2026,9,11방재실 09:12

--- lib/Model/schedule.dart
+++ lib/Model/schedule.dart
@@ -5,26 +5,32 @@ class Schedule extends Table  {
   /// 그다음 property, 즉 필드를 만들기
   /// -----------------------------------
   /// 1) 식별 가능한 ID
-  final int id1;
-  IntColumn get id=>integer()();
+  //final int id;
+  IntColumn get id=>integer().autoIncrement()();
 
   /// 2) 시작시간
-  final int startTime;
+  //final int startTime;
+  IntColumn get startTime=>integer()();
 
   /// 3) 종료시간
-  final int endTime;
+  //final int endTime;
+  IntColumn get endTime=>integer()();
 
   /// 4) 일정내용
-  final String content;
+  //final String content;
+  TextColumn get content=>text()();
 
   /// 5) 날짜
-  final DateTime date;
+  //final DateTime date;
+  DateTimeColumn get date=>dateTime()();
 
   /// 6) 카테고리
-  final String color;
+  //final String color;
+  TextColumn get color=>text()();
 
   /// 7) 일정 생성날짜
-  final DateTime createdAt;
+  //final DateTime createdAt;
+  DateTimeColumn get createdAt=> dateTime().clientDefault(()=>DateTime.now().toUtc())();
 
   Schedule({
     required this.id,
