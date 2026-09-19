2e24407794acdbe5b426e7fe867ed5a9b517071d
cwpark <beamwidth@naver.com>
2026-09-19 12:55:58 +0900

    2026,9,19 방재실 12:55

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cw_calendar_tr11/database/drift.dart';
 import 'package:cw_calendar_tr11/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -8,6 +9,10 @@ void main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting();
+
+  final  database=AppDatabase();
+
+
 
   runApp(
     MaterialApp(
